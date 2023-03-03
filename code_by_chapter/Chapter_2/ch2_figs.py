f71d051bf1a86964381a3d5b1522dfebd882e5f6
loveLH3399 <125754593+loveLH3399@users.noreply.github.com>
2023-03-03 10:04:48 +0100

    Update ch2_figs.py

--- code_by_chapter/Chapter_2/ch2_figs.py
+++ code_by_chapter/Chapter_2/ch2_figs.py
@@ -19,13 +19,13 @@ import matplotlib.pyplot as plt
 
 # initialize
 n_trials = 20000
-b = [3, 0.2]
-w_p = 0.01
+b = [3, 0.2] # 定义后内容可变，里面有append（）方法 ##分别是猫和狗的input值
+w_p = 0.01 # weight的初始值
 w_m = 1
 theta = 0.995 # 1-decay parameter
-alpha = 0.01 # change rate
+alpha = 0.01 # change rate 学习率
 max_n_step = 300
-std_noise = 0.1
+std_noise = 0.1 #random
 threshold=2
 rt = []
 accuracy = too_late = 0
@@ -40,15 +40,15 @@ for loop in range(n_trials):
     while not threshold_reached and counter<max_n_step:
         d_x = alpha*(b[0] + 2*w_p*x[-1] - w_m*y[-1]) + std_noise*random.randn()
         d_y = alpha*(b[1] + 2*w_p*y[-1] - w_m*x[-1]) + std_noise*random.randn()
-        x.append(theta*x[-1] + d_x) # add an element to the end of a list
+        x.append(theta*x[-1] + d_x) # add an element to the end of a list ##同步赋值更新
         y.append(theta*y[-1] + d_y)
-        x[-1] = maximum(0,x[-1]) # threshold at zero
+        x[-1] = maximum(0,x[-1]) # threshold at zero 猫/狗达到负值，停止运行
         y[-1] = maximum(0,y[-1]) # threshold at zero
         if maximum(x[-1],y[-1])>=threshold:
             threshold_reached = True
         counter += 1    
     rt.append(counter)# reaction time # rt存储所有实验的counter
-    accuracy += (x[-1]>y[-1]) # the proportion of trials where x was higher than y
+    accuracy += (x[-1]>y[-1]) # the proportion of trials where x was higher than y ## 猫大于狗（在运行过程中 可能会有相反的中间过程）
     too_late += (counter>=max_n_step)
 
 # plot final trajectory
