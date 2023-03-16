64f862c78b38cd1e63bae06cea46e308d4b4befb
loveLH3399 <125754593+loveLH3399@users.noreply.github.com>
2023-03-16 23:42:51 +0100

    Update ch5_linear_mappings.py

--- code_by_chapter/Chapter_5/ch5_linear_mappings.py
+++ code_by_chapter/Chapter_5/ch5_linear_mappings.py
@@ -9,15 +9,14 @@ convenient to make slides illustrating how backprop works
 """
 
 import matplotlib.pyplot as plt
-import numpy as np
+import numpy as np # x和y值通过numpy中的linspace函数生成，它们在指定的范围内生成一系列均匀分布的数值
 
-w = np.array([[1, 1, 0], [2, -1, 2], [-1.5, 2, 3], [-1, -1, -1]])
-
+w = np.array([[1, 1, 0], [2, -1, 2], [-1.5, 2, 3], [-1, -1, -1]]) # 数组"w"实际上是一个4x3的矩阵（或二维数组），其中每一行是一个向量，表示一个线性切面的系数。具体来说，每个向量包含三个元素，前两个元素是该切面方程中x和y变量的系数，第三个元素是常数项。
 fig, axs = plt.subplots(nrows = 2, ncols = 2)
 
 x = np.linspace(-1, 1)
 
-for loop in range(4):
+for loop in range(4): # 代码中的for循环通过遍历数组“w”的每一行，生成对应的x和y值，并使用pyplot的plot函数将它们绘制在图形上。
 	row = loop//2
 	col = loop%2
 	y = w[loop, 0]*x + w[loop, 1]
