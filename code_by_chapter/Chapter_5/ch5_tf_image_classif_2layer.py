345164bc675ac75cbb0afa96f28f6b9fbb779626
loveLH3399 <125754593+loveLH3399@users.noreply.github.com>
2023-03-16 23:35:48 +0100

    Update ch5_tf_image_classif_2layer.py

--- code_by_chapter/Chapter_5/ch5_tf_image_classif_2layer.py
+++ code_by_chapter/Chapter_5/ch5_tf_image_classif_2layer.py
@@ -7,11 +7,11 @@ Created on Wed Sep  2 14:49:49 2020
 image classification; could a standard two-layer network solve this task...?
 """
 
-import tensorflow as tf
+import tensorflow as tf # CIFAR-10 dataset, which consists of 60,000 32x32 color images in 10 classes, with 6,000 images per class
 import numpy as np
 import matplotlib.pyplot as plt
 
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data() # load the CIFAR-10 dataset
 
 
 # for plotting
@@ -23,12 +23,12 @@ for img, label, ax in zip(x_train[:4], y_train[:4], axes):
 plt.show()
 
 # pre-processing
-n_labels = int(np.max(y_train)+1)
+n_labels = int(np.max(y_train)+1) # 计算数据有多少标签
 image_size = x_train.shape[1]*x_train.shape[2]*x_train.shape[3]
-x_train = x_train.reshape(x_train.shape[0], image_size)  / 255
+x_train = x_train.reshape(x_train.shape[0], image_size)  / 255 # /255 得到[0,1]
 x_test  = x_test.reshape(x_test.shape[0], image_size)    / 255
-y_train = y_train[:,0] # remove a dimension
-y_test  = y_test[:,0]
+y_train = y_train[:,0] # remove a dimension把第1行删掉，留下第0行
+y_test  = y_test[:,0] # Convert the target labels into one-hot encoded vectors使用N位状态寄存器来对N个状态进行编码，每个状态都有它独立的寄存器位，并且在任意时候，其中只有一位有效
 
 # for piloting
 x_train, y_train, x_test, y_test = x_train[:5,:], y_train[:5], x_test[:100,:], y_test[:100]
@@ -41,19 +41,19 @@ learning_rate = 0.001
 epochs = 10
 batch_size = 100
 batches = int(x_train.shape[0] / batch_size)
-stdev = 0.001
+stdev = 0.001 # 学习率、迭代次数、批次大小和标准差
 
-X   = tf.placeholder(tf.float32, [None, image_size])
+X   = tf.placeholder(tf.float32, [None, image_size])# X 表示输入数据的占位符，Y 表示目标标签的占位符，W 和 B 分别表示模型的权重和偏置
 Y   = tf.placeholder(tf.float32, [None, n_labels])
 W   = tf.Variable(np.random.randn(image_size, n_labels).astype(np.float32)*stdev)
 B   = tf.Variable(np.random.randn(n_labels).astype(np.float32))
 
-net_in  = tf.add(tf.matmul(X, W), B)
-net_inT    = 1/(1+tf.math.exp(-net_in)) # hidden transformed : Note: this is not softmax
-pred = tf.nn.softmax(net_inT)
-cost = tf.reduce_mean(-tf.reduce_sum(Y*tf.math.log(pred), axis = 1))
-opt  = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
-init = tf.global_variables_initializer()
+net_in  = tf.add(tf.matmul(X, W), B) # This line performs the forward pass of the neural network by computing the weighted sum of the input features X (which is a tensor三维变量) using the weights W and adding the bias B. This operation produces a new tensor net_in which is the input to the activation function.
+net_inT    = 1/(1+tf.math.exp(-net_in)) # hidden transformed : Note: this is not softmax ## input压缩在 0 和 1 之间
+pred = tf.nn.softmax(net_inT) # 产生每个类别的预测概率
+cost = tf.reduce_mean(-tf.reduce_sum(Y*tf.math.log(pred), axis = 1)) # 定义成本函数
+opt  = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost) # 梯度下降最小化成本函数
+init = tf.global_variables_initializer() # 这行代码在训练过程开始之前初始化模型中使用的所有变量，如权重 W 和偏置 B
 
 with tf.Session() as sess:
     sess.run(init)
